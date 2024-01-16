6d81250c893ef64b0738073d658da7e44aa57a66
Yasin Arafat <mdyasinarafat3@gmail.com>
2024-01-16 07:38:31 +0100

    Create can_receive2.c

---
+++ Task3/can_receive2.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <net/if.h>
+#include <iostream>
+using namespace std;
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+int main(void)
+{
+    int s;
+    int nbytes;
+    struct sockaddr_can addr;
+    struct can_frame frame;
+    struct ifreq ifr;
+
+    const char *ifname = "can0";
+
+    if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) == -1)
+    {
+        perror("Error while opening socket");
+        return -1;
+    }
+
+    strcpy(ifr.ifr_name, ifname);
+    ioctl(s, SIOCGIFINDEX, &ifr);
+
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+
+    printf("%s at index %d\n", ifname, ifr.ifr_ifindex);
+
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        perror("Error in socket bind");
+        return -2;
+    }
+
+    while (1)
+    {
+        nbytes = read(s, &frame, sizeof(struct can_frame));
+        printf("%d \n", frame.can_id);
+
+        if (frame.can_id == 291 && frame.data[0] == 1)
+        {
+            printf("Human Detected \n");
+        }
+        else if (frame.can_id == 801 && frame.data[0] == 1)
+        {
+            printf("Others Detected \n");
+        }
+        else
+        {
+            printf("Continue \n");
+        }
+        // cout << frame.data[0] << endl;
+    }
+    return 0;
+}
